2fe0a6aa1656fcd48b92408e90c790ce206f0a87
holawan <asdf134652@gmail.com>
2022-06-19 15:28:32 +0900

    [fix] usermodel 생성, signup test

---
+++ accounts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers 
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+class SignupSerializer(serializers.Serializer) :
+
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    def create(self,validated_data) :
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        print('dd')
+        user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404 
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
+
+from .serializers import SignupSerializer
+
 
 # Create your views here.
+
+User = get_user_model()
+
+@api_view(['POST'])
+def signup(request) :
+    if request.method == 'POST' :
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True) :
+            serializer.save()
+            return Response({'message':'ok'},status=status.HTTP_201_CREATED)
+        else :
+            return Response({"message" : "Request Body Error"},status=status.HTTP_409_CONFLICT)
