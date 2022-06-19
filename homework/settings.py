8837b1539e2a13c22fbd088ce9fbaf41ba58ac9c
holawan <asdf134652@gmail.com>
2022-06-19 17:01:35 +0900

    [FEAT] swagger

--- homework/settings.py
+++ homework/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'drf_yasg',
     'rest_framework',
     'imagekit',
     'django_seed'
--- homework/urls.py
+++ homework/urls.py
@@ -12,11 +12,28 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""#ursl.py
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path,include
 
+schema_view = get_schema_view(
+   openapi.Info(
+    #필수인자
+      title="Snippets API Practice",
+      default_version='v1',
+    
+    #아래부터는 선택인자 
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/',include('accounts.urls')),
+    path('swegger/', schema_view.with_ui('swagger')),
 ]
